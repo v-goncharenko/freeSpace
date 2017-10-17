1d1dc940977b68ba7d910b908d38853585d5e8e4
V G <sgt.pepper.me@gmail.com>
2017-10-17 16:49:16 +0300

    added doppler test, time meaurement

--- generateTests.m
+++ generateTests.m
@@ -9,15 +9,20 @@ function generateTests()
     origin_vel = [0;0;0];
     dest_vel = [0;0;0];
     signal = ones(5,1);
-    save('testCases/1_input', 'signal', 'operating_frequency', 'sample_rate', 'origin_pos', 'dest_pos', 'origin_vel', 'dest_vel');
+    save('test_cases/1_input', 'signal', 'operating_frequency', 'sample_rate',...
+        'origin_pos', 'dest_pos', 'origin_vel', 'dest_vel');
 
-    henv = phased.FreeSpace('SampleRate', sample_rate, 'OperatingFrequency', operating_frequency);
+    henv = phased.FreeSpace('SampleRate', sample_rate,...
+        'OperatingFrequency', operating_frequency);
+    tic
     y = step(henv, signal, ...
         origin_pos, ...
         dest_pos,...
         origin_vel,...
         dest_vel);
-    save('testCases/1_matlab', 'y')
+    disp('#1 step')
+    toc
+    save('test_cases/1_matlab', 'y')
 
     % case #2 FM wave
     waveform = phased.LinearFMWaveform('SweepBandwidth',1e5,...
@@ -30,12 +35,16 @@ function generateTests()
     dest_pos = [3000; 750; 20];
     origin_vel = [0;0;0];
     dest_vel = [0;0;0];
-    save('testCases/2_input', 'signal', 'operating_frequency', 'sample_rate', 'origin_pos', 'dest_pos', 'origin_vel', 'dest_vel');
+    save('test_cases/2_input', 'signal', 'operating_frequency', 'sample_rate',...
+        'origin_pos', 'dest_pos', 'origin_vel', 'dest_vel');
     
     channel = phased.FreeSpace('SampleRate',sample_rate,...
         'TwoWayPropagation',false,'OperatingFrequency',operating_frequency);
+    tic
     y = channel(signal, origin_pos, dest_pos, origin_vel, dest_vel);
-    save('testCases/2_matlab', 'y');    
+    disp('#2 step')
+    toc
+    save('test_cases/2_matlab', 'y');    
     
     % case #3 long signal
     sample_rate = 8e3;
@@ -45,15 +54,16 @@ function generateTests()
     origin_vel = [0;0;0];
     dest_vel = [0;0;0];
     signal = ones(1e6,1);
-    save('testCases/3_input', 'signal', 'operating_frequency', 'sample_rate', 'origin_pos', 'dest_pos', 'origin_vel', 'dest_vel');
+    save('test_cases/3_input', 'signal', 'operating_frequency', 'sample_rate',...
+        'origin_pos', 'dest_pos', 'origin_vel', 'dest_vel');
 
-    henv = phased.FreeSpace('SampleRate', sample_rate, 'OperatingFrequency', operating_frequency);
-    y = step(henv, signal, ...
-        origin_pos, ...
-        dest_pos,...
-        origin_vel,...
-        dest_vel);
-    save('testCases/3_matlab', 'y')
+    henv = phased.FreeSpace('SampleRate', sample_rate,...
+        'OperatingFrequency', operating_frequency);
+    tic
+    y = step(henv, signal, origin_pos, dest_pos, origin_vel, dest_vel);
+    disp('#3 step')
+    toc
+    save('test_cases/3_matlab', 'y')
     
     % case #4 double step
     waveform = phased.LinearFMWaveform('SweepBandwidth',1e5,...
@@ -67,12 +77,37 @@ function generateTests()
     dest_pos = [3000; 750; 20];
     origin_vel = [0;0;0];
     dest_vel = [0;0;0];
-    save('testCases/4_input', 'signal_1', 'signal_2', 'operating_frequency', 'sample_rate', 'origin_pos', 'dest_pos', 'origin_vel', 'dest_vel');
+    save('test_cases/4_input', 'signal_1', 'signal_2', 'operating_frequency',...
+        'sample_rate', 'origin_pos', 'dest_pos', 'origin_vel', 'dest_vel');
     
     channel = phased.FreeSpace('SampleRate',sample_rate,...
         'TwoWayPropagation',false,'OperatingFrequency',operating_frequency);
+    tic
     y_1 = channel(signal_1, origin_pos, dest_pos, origin_vel, dest_vel);
     y_2 = channel(signal_2, origin_pos, dest_pos, origin_vel, dest_vel);
-    save('testCases/4_matlab', 'y_1', 'y_2');    
+    disp('#4 step')
+    toc
+    save('test_cases/4_matlab', 'y_1', 'y_2');
+    
+    % case #5 doppler
+    waveform = phased.LinearFMWaveform('SweepBandwidth',1e5,...
+        'PulseWidth',5e-5,'OutputFormat','Pulses',...
+        'NumPulses',1,'SampleRate',1e6,'PRF',1e4);
+    signal = waveform();
+    operating_frequency = 1e9;
+    sample_rate = 1e6;
+    origin_pos = [1000; 250; 10];
+    dest_pos = [3000; 750; 20];
+    origin_vel = [100; 30; 25];
+    dest_vel = [0; 6; -80];
+    save('test_cases/5_input', 'signal', 'operating_frequency', 'sample_rate',...
+        'origin_pos', 'dest_pos', 'origin_vel', 'dest_vel');
+    
+    channel = phased.FreeSpace('SampleRate',sample_rate,...
+        'TwoWayPropagation',false,'OperatingFrequency',operating_frequency);
+    tic
+    y = channel(signal, origin_pos, dest_pos, origin_vel, dest_vel);
+    disp('#5 step')
+    toc
+    save('test_cases/5_matlab', 'y');        
 end
-
